5d35b80d362c070214cd05b9b87ac3599444de7e
Khach Tonoyan <tonoyan.khachik@gmail.com>
2020-07-13 13:26:08 +0400

    add DnD

--- App/List/ListView.js
+++ App/List/ListView.js
@@ -9,49 +9,79 @@ mainDiv.classList.add("list")
 
 //adding new section was causing a complete update that resulted in a user data loss
 //push to list (state)
+let draggedItem;
 function render(element) {
     const newSection = renderUtil(element);
 
     mainDiv.appendChild(newSection);
     //list.push...
 }
-
+function createDraggable(element){
+    element.draggable = true;
+    element.id = Date.now();
+    element.classList.add("draggableItem");
+    element.ondragstart = function(){
+        draggedItem = this;
+        setTimeout(() => this.classList.add("hide"))
+    }
+    element.ondragend = function(){
+        this.classList.remove("hide")
+    }
+}
+function createDragArea(element){
+    element.classList.add("dragArea")
+    element.ondragover = function(e){
+        e.preventDefault()
+    }
+    element.ondragenter = function(){
+        this.classList.add("dragEnter")
+        console.log("enter")
+    }
+    element.ondragleave = function(){
+        this.classList.remove("dragEnter")
+        console.log("leave")
+    }
+    element.ondrop = function(){
+        this.append(draggedItem)
+        this.classList.remove("dragEnter")
+    }
+}
 //moved this function out of the "render" to keep the code cleaner
 //currently box id is missing
 function renderUtil(element) {
     const header = document.createElement("p"); //name of the section
-    header.textContent = `${element.value}`
+    header.textContent = element.value
 
     const category = document.createElement("div");
     category.classList.add("cat_main")
     category.appendChild(header); //add name
-
+    const tasksBox = document.createElement("div")
+    createDragArea(tasksBox)
     //delete this
     const taskInputBox = document.createElement("div");
-    const input_id = new Date().getTime();
+    const input_id = Date.now();
     taskInputBox.innerHTML = `<input type="text" id=${input_id}><br>`
 
     //when user clicks enter his task is being added to the top of the current container
     taskInputBox.addEventListener('keypress', (event) => {
         if (event.key === 'Enter') {
             const taskInput = document.getElementById(input_id);
-
-                                                        //this node can use ItemView as a constructor
-                                                        const bTest = document.createElement("button");
-                                                        bTest.innerText = "X"
-                                                        bTest.onclick = function foo(){
-                                                            this.parentNode.remove();
-                                                        }
-                                                        const pTest = document.createElement("p");
-                                                        pTest.textContent =taskInput.value;
-                                                        pTest.appendChild(bTest)
-            // taskInputBox.insertAdjacentHTML('afterend',)
-            taskInputBox.appendChild(pTest)
+            //this node can use ItemView as a constructor
+            const deleteButton = document.createElement("button");
+            deleteButton.innerText = "X"
+            deleteButton.onclick = function foo(){
+                this.parentNode.remove();
+            }
+            const item = document.createElement("p");
+            createDraggable(item)
+            item.textContent =taskInput.value;
+            item.appendChild(deleteButton)
+            tasksBox.appendChild(item)
             taskInput.value = "";
         }
     })
 
-    category.appendChild(taskInputBox);
+    category.append(taskInputBox,tasksBox);
     return category;
 }
 
